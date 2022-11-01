32d15c676dabceac7ae1d6659e4b12ccb7a051ca
cksghks89 <song960705@naver.com>
2022-11-02 06:15:40 +0900

    init mini-LMS project

---
+++ src/main/java/com/example/minilms/MiniLmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.minilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniLmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minilms/MiniLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minilms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
